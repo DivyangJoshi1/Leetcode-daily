0f492f34e46acd440995eb6ed4e7908958a01aa0
DivyangJoshi1 <divyangjoshi94247@gmail.com>
2025-03-04 03:13:10 +0530

    commit

--- 009_Palindrome_Number/python/palindrome_number_string.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        """
-        Convert number to string and check if it reads the same backward.
-        """
-        return str(x) == str(x)[::-1]
