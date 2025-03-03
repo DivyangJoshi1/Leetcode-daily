bbe3078d3cf06fe17b70cbc73d6bcd39b9ced334
DivyangJoshi1 <divyangjoshi94247@gmail.com>
2025-03-04 03:29:39 +0530

    commit

---
+++ 0053_Maximum_Subarray/java/Divide_and_Conquer.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        return maxSubArrayHelper(nums, 0, nums.length - 1);
+    }
+
+    private int maxSubArrayHelper(int[] nums, int left, int right) {
+        if (left == right) return nums[left];
+
+        int mid = left + (right - left) / 2;
+        int leftMax = maxSubArrayHelper(nums, left, mid);
+        int rightMax = maxSubArrayHelper(nums, mid + 1, right);
+        int crossMax = maxCrossingSum(nums, left, mid, right);
+
+        return Math.max(leftMax, Math.max(rightMax, crossMax));
+    }
+
+    private int maxCrossingSum(int[] nums, int left, int mid, int right) {
+        int leftSum = Integer.MIN_VALUE, rightSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum + rightSum;
+    }
+}
